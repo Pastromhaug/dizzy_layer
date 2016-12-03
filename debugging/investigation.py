e042495c8f7ce63c79e85cfb65de7f1157d4771a
Per Andre Stromhaug <pas282@cornell.edu>
2016-12-03 15:36:06 -0500

    fd

--- debugging/investigation.py
+++ debugging/investigation.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import sys
 from tensorflow.python.client import timeline
 
-thetas = tf.Variable(tf.random_uniform([n_prime, 1], 0, 2*math.pi), name="thetas", dtype=tf.float32)
+thetas = tf.Variable(tf.random_uniform([5,5], 0, 2*math.pi), name="thetas", dtype=tf.float32)
 cos = tf.cos(thetas)
 
 train_step = tf.train.AdagradOptimizer(learning_rate).minimize(loss + regularization_loss)
@@ -14,5 +14,5 @@ run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
 train_step_ = sess.run([train_step], options=run_options, run_metadata=run_metadata)
 tl = timeline.Timeline(run_metadata.step_stats)
 ctf = tl.generate_chrome_trace_format()
-with open('./timelines/additionV1.json', 'w') as f:
+with open('./timelines/cos.json', 'w') as f:
     f.write(ctf)
