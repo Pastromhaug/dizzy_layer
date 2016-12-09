3c89e05265cc65b9db5159b729bbd605371b5f77
jessrenteria <jvr35@cornell.edu>
2016-12-08 21:09:13 -0500

    changed regularization to mean, not sum STRAIGHT IN MASTER

--- utils/regularizeSpread.py
+++ utils/regularizeSpread.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
 def regularizeSpread(sigma, Lambda):
-    L_sigma = 0.5 * Lambda * tf.reduce_sum(tf.square(sigma - 1))
+    L_sigma = 0.5 * Lambda * tf.reduce_mean(tf.square(sigma - 1))
     return L_sigma
